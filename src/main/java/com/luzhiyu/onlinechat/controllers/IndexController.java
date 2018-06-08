e2c5fe79aa493aea32fc9edb2f7b67cd3d41929a
bigexin <498611438@qq.com>
2018-06-08 10:01:22 +0800

    优化了多余的引用

--- src/main/java/com/luzhiyu/onlinechat/controllers/IndexController.java
+++ src/main/java/com/luzhiyu/onlinechat/controllers/IndexController.java
@@ -1,17 +1,12 @@
 package com.luzhiyu.onlinechat.controllers;
 
 import com.luzhiyu.onlinechat.SessionConstants;
-import com.luzhiyu.onlinechat.entity.CommonRes;
 import com.luzhiyu.onlinechat.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class IndexController {
